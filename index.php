55532c30a663234ce885a0c915a28c80447ad582
orpimel <i908889@mvrht.com>
2016-11-11 15:19:41 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!-- PARA MAS INFORMACION CONSULTAR http://josehernandez.xyz/2010/10/02/uso-json-javascript.html -->
+
+<!DOCTYPE html>
+
+<html lang="es">
+
+  <head>
+
+    <title>prueba</title>
+
+    <script type="text/javascript">
+
+      var nemo = new Array();
+
+      // nemo = {"uno":"colombia", "dos":"mexico"};
+
+      nemo = ["uno", "tres", "cuatro"];
+
+      for (var i in nemo) {
+
+        alert(nemo[i]);
+
+      }
+
+    </script>
+
+  </head>
+
+  <body>
+
+  </body>
+
+</html>
